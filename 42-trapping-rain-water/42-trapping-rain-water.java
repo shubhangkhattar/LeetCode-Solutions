1ca52d5eff07a17df52c1fae3178be9d306b54b4
Shubhang Khattar <shubhang1995khattar@gmail.com>
2022-02-24 00:00:31 -0500

    Time: 0 ms (100.00%), Space: 43.2 MB (35.37%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,28 +1,35 @@
 class Solution {
 	public int trap(int[] height) {
 
-		int suffix[] = new int[height.length];
-		int prefix[] = new int[height.length];
-		int suffix_max = 0;
-		int prefix_max = 0;
 		int volume = 0;
 
-		for (int i = 0; i < height.length; i++) {
+		int n = height.length;
 
-			suffix[i] = Math.max(suffix_max, height[i]);
-			suffix_max = suffix[i];
-
-			prefix[height.length - 1 - i] = Math.max(prefix_max, height[height.length - 1 - i]);
-			prefix_max = prefix[height.length - 1 - i];
-
+		int left = 0, right = n - 1;
+		
+		int maxLeft = 0 , rightMax = 0;
+		
+		while(left <= right) {
+			
+			if(height[left] <= height[right]) {
+				if(height[left] >= maxLeft) 
+					maxLeft = height[left];
+				else
+					volume += maxLeft - height[left];
+				
+				left++;
+				
+			} else {
+				if(height[right] >= rightMax)
+					rightMax = height[right];
+				else 
+					volume += rightMax - height[right];
+				
+				right--;
+			}
+			
 		}
-
-		for (int i = 0; i < height.length; i++) {
-
-			volume += Math.min(suffix[i], prefix[i]) - height[i];
-
-		}
-
+		
 		return volume;
 	}
 
