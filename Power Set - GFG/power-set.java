4b63188018199f90291b98dbcf0378dc2bffcedf
Shubhang Khattar <shubhang1995khattar@gmail.com>
2022-03-26 09:02:22 -0400

    Added solution - LeetHub

---
+++ Power Set - GFG/power-set.java
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String s = br.readLine().trim();
+            Solution ob = new Solution();
+            List<String> ans = ob.AllPossibleStrings(s);
+            for(String i: ans)
+                System.out.print(i + " ");
+            System.out.println();
+            
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+	public List<String> AllPossibleStrings(String s) {
+
+		List<String> result = new ArrayList<>();
+		
+		int n = s.length();
+
+		for (int num = 0; num < (1 << n); num++) {
+
+			String substring = "";
+
+			for (int i = 0; i < n; i++) {
+
+				if ((num & (1 << i)) != 0) {
+
+					substring += s.charAt(i);
+
+				}	
+
+			}
+			
+			if (substring.length() > 0) {
+				result.add(substring);
+			}
+
+		}
+		
+		Collections.sort(result);
+
+		return result;
+	}
+}
