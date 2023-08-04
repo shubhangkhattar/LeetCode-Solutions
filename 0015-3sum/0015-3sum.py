f984f18d838a107ccf23f4dd40a3ec81a6825128
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-08-04 19:07:32 -0400

    Time: 2763 ms (18.29%), Space: 16.4 MB (96.97%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        result = set()
+
+        nums.sort()
+        n = len(nums)
+        for i in range(0,n-2):
+
+            left = i+1
+            right = n-1
+
+            while left < right:
+                if nums[left] + nums[right] == -nums[i]:
+                    result.add((nums[i],nums[left],nums[right]))
+                if -nums[i] < (nums[left] + nums[right]):
+                    right -= 1
+                else:
+                    left += 1
+        return list(result)
