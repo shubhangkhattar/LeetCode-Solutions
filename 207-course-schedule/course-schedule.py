4eb19e61fae95b15e69098d6e2790c8756d04670
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-08-14 21:05:17 -0400

    Time: 95 ms (94.76%) | Memory: 19.2 MB (34.68%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        pre = defaultdict(list)
+        taken = set()
+
+        for course, p in prerequisites:
+            pre[course].append(p)
+
+        def dfs(course):
+            if not pre[course]:
+                return True
+
+            if course in taken:
+                return False
+
+            taken.add(course)
+
+            for p in pre[course]:
+                if not dfs(p):
+                    return False
+
+            pre[course] = []
+
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True
+
