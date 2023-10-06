5929bca148e0e867738f6289b3c7541febf059f3
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-06 02:02:43 -0400

    Time: 145 ms (9.43%), Space: 16.4 MB (72.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,6 +1,11 @@
 class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        self.dfs(0,candidates,target,[],result)
+        return result
 
-    def dfs(self,index,candidates:List[int],target:int, current_list:List[int],result: List[List[int]]):
+    def dfs(self, index, candidates, target, current_list, result):
+        print(current_list)
         if sum(current_list) == target:
             result.append(current_list)
             return
@@ -10,11 +15,7 @@ class Solution:
         increased_list = current_list.copy()
         increased_list.append(candidates[index])
 
-        self.dfs(index, candidates, target, increased_list, result)
-        if index + 1 < len(candidates):
-            self.dfs(index+1, candidates, target, current_list, result)
+        self.dfs(index, candidates, target, increased_list,result)
 
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
-        self.dfs(0,candidates,target,[],result)
-        return result
+        if index+1 < len(candidates):
+            self.dfs(index+1,candidates,target,current_list,result)
