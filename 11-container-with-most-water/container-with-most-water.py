056c7df68099dab063d7abd5aeed7207b87eb47d
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-05 22:35:22 -0400

    Time: 613 ms (25.44%) | Memory: 29.2 MB (69.37%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,15 +1,18 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        left = 0
-        right = len(height)-1
-        max_volume = 0
-        while left < right and left < len(height) and right > 0:
-            tank_height = min(height[left],height[right])
-            tank_width = right-left
-            tank_volume = tank_height * tank_width
-            max_volume = max(tank_volume,max_volume)
+
+        left = 0 
+        right = len(height) - 1
+        max_vol = 0
+        while left < right:
+            min_height = min(height[left],height[right])
+            min_width = right - left
+            volume = min_width * min_height
+            max_vol = max(volume,max_vol)
+            
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
-        return max_volume
+                
+        return max_vol
