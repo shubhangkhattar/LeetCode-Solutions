de278e7aa4eeda9bf9c2f8d5aec22901b30714df
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-05 21:58:44 -0400

    Time: 2782 ms (18.13%) | Memory: 16.3 MB (94.74%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,19 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-
         result = set()
 
         nums.sort()
         n = len(nums)
-        for i in range(0,n-2):
+        for i in range(0, n - 2):
 
-            left = i+1
-            right = n-1
+            left = i + 1
+            right = n - 1
 
             while left < right:
                 if nums[left] + nums[right] == -nums[i]:
-                    result.add((nums[i],nums[left],nums[right]))
+                    result.add((nums[i], nums[left], nums[right]))
                 if -nums[i] < (nums[left] + nums[right]):
                     right -= 1
                 else:
