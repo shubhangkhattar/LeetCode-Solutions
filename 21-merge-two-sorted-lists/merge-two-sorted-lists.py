914c0e6db9e8add50937034c3116195f87b0a833
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-07 19:07:26 -0400

    Time: 51 ms (10.26%) | Memory: 16.3 MB (34.76%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -1,15 +1,13 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        head = None
+
 
         if list1 == None:
             return list2
@@ -17,29 +15,33 @@ class Solution(object):
             return list1
 
         if list1.val <= list2.val:
-            new_head = list1
+            head = list1
             list1 = list1.next
-
         else:
-            new_head = list2
+            head = list2
             list2 = list2.next
 
-        ans = new_head
+        ans = head
 
-        while (list1 != None) and (list2 != None):
+        while list1 != None and list2 != None:
             if list1.val <= list2.val:
-                new_head.next = list1
-                list1 = list1.next
+                head.next = list1
+                list1 =  list1.next
             else:
-                new_head.next = list2
-                list2 = list2.next
-
-            new_head = new_head.next
+                head.next = list2
+                list2 =  list2.next
+            head = head.next
 
 
         if list1 != None:
-            new_head.next = list1
-        elif list2 != None:
-            new_head.next = list2
+            head.next = list1
+        
+        if list2 != None:
+            head.next = list2
+
 
         return ans
+
+
+
+        
