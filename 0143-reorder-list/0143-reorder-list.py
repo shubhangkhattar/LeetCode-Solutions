b593c26af65e6977dd420842925d280217f5c62a
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-07 23:31:24 -0400

    Time: 78 ms (64.43%), Space: 26 MB (76.87%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.py
@@ -0,0 +1,43 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+
+        slow, fast = head, head.next
+
+        while fast and fast.next :
+            slow = slow.next
+            fast = fast.next.next
+        
+        second = slow.next
+        slow.next = None
+
+        first = head
+        second = self.reverse(second)
+
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
+
+
+    
+    def reverse(self,head):
+
+        prev = None
+
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        
+        return prev
+                
