8724d01630a71e3ebeca607f6d228c475c54c88b
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-08 01:45:38 -0400

    Time: 62 ms (56.32%), Space: 16.2 MB (99.03%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,32 +1,22 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if len(s) == 1:
-            return 1
-
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
         front = 0
         back = 0
 
-        current_set = set()
 
-        max_length = 0
+        current_set = set()
+        max_count = 0
 
         while front < len(s):
+
             if s[front] not in current_set:
                 current_set.add(s[front])
-                max_length = max(len(current_set),max_length)
+                max_count = max(max_count,len(current_set))
                 front += 1
+
             else:
                 current_set.remove(s[back])
                 back += 1
 
-        return max_length
 
-
-if __name__ == "__main__":
-    str = "pwwkew"
-
-    print("The length of the longest substring without repeating characters is", Solution().lengthOfLongestSubstring(str))
+        return max_count
