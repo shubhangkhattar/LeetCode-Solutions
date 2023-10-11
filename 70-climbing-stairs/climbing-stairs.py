407832d2ce914c4d84c380a8edeabd733ef3ecd3
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-11 11:36:05 -0400

    Time: 30 ms (93.05%) | Memory: 16.1 MB (79.93%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -2,7 +2,7 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         first,two = 1,1
 
-        for i in range(n):
+        for i in range(n-1):
             two,first = two + first, two
         
-        return first
+        return two
