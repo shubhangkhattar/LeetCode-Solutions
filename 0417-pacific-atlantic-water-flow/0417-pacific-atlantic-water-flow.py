a870f704fdb7659d22a4a79310d441c2451a6ccc
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-13 15:14:50 -0400

    Time: 248 ms (48.07%), Space: 17.9 MB (68.63%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -5,22 +5,19 @@ class Solution:
 
         pac, atl = set(),set()
 
-        def dfs(r,c, ocean, prev_height):
-
-            if  (r,c) in ocean or r == ROWS or r < 0 or c == COLS or c < 0  or prev_height > heights[r][c]:
+        def dfs(r,c,ocean,prev):
+            if (r,c) in ocean or r == ROWS or c == COLS or r < 0 or c < 0 or heights[r][c] < prev:
                 return
-
+                    
             ocean.add((r,c))
-
             dfs(r+1,c,ocean,heights[r][c])
             dfs(r-1,c,ocean,heights[r][c])
             dfs(r,c+1,ocean,heights[r][c])
             dfs(r,c-1,ocean,heights[r][c])
 
-
         for c in range(COLS):
-            dfs(0,c, pac, heights[0][c])
-            dfs(ROWS-1,c, atl, heights[ROWS-1][c])
+            dfs(0,c,pac,heights[0][c])
+            dfs(ROWS-1,c,atl,heights[ROWS-1][c])
 
         for r in range(ROWS):
             dfs(r,0, pac, heights[r][0])
@@ -32,5 +29,7 @@ class Solution:
             for c in range(COLS):
                 if (r,c) in pac and (r,c) in atl:
                     res.append([r,c])
-                    
+        
         return res
+
+
