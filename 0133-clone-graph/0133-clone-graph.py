70c6f5703690bf0d44ac7c1c72eddce09390b353
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-13 14:28:10 -0400

    Time: 44 ms (58.95%), Space: 16.6 MB (77.16%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -6,19 +6,26 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
+from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        
+        if not node:
+            return 
+        
         my_map = {}
-
+        
         def dfs(node):
             if node in my_map:
                 return my_map[node]
-            print(node)
+            
             new_node = Node(node.val)
             my_map[node] = new_node
 
             for neighbor in node.neighbors:
                 new_node.neighbors.append(dfs(neighbor))
+
             return new_node
 
-        return dfs(node) if node else None
+        return dfs(node)
+
