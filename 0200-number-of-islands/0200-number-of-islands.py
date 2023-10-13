7b40bfe68e1f3b962a46f89d5556c65a26a5b55e
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-13 15:28:55 -0400

    Time: 296 ms (53.01%), Space: 18.8 MB (83.24%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        visited = [[-1]*len(grid[0]) for i in range(len(grid))]
-        ROWS, COLS = len(grid) , len(grid[0])
- 
+        
+        ROWS, COLS = len(grid),len(grid[0])
+
+        visited = [[-1] * len(grid[0]) for i in range(len(grid)) ]
+        
+        count = 0
+
         def dfs(r,c):
-            if r < 0 or c < 0 or r == ROWS or c == COLS or grid[r][c] == "0" or visited[r][c] == 1:
-                return
+            if (r >= ROWS or r < 0 or c >= COLS or c < 0 or grid[r][c] != "1"  or visited[r][c] == 1):
+                return 
             visited[r][c] = 1
-            
-            dfs(r-1,c)
             dfs(r+1,c)
-            dfs(r,c-1)
+            dfs(r-1,c)
             dfs(r,c+1)
+            dfs(r,c-1)
+        
+        for i in range(ROWS):
+            for j in range(COLS):
+                if grid[i][j] == "1" and visited[i][j] == -1:
+                    count += 1
+                    dfs(i,j)
 
-        ans = 0
-        print()
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c] == "1" and visited[r][c] == -1:
-                    ans +=1
-                    dfs(r,c)
-
-        return ans
+        return count
