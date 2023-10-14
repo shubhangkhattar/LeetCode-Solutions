c73671763d73d770f2babe630a27729b14325a88
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-10-14 16:53:27 -0400

    Time: 81 ms (47.34%), Space: 26 MB (74.41%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -1,43 +1,41 @@
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
         Do not return anything, modify head in-place instead.
         """
 
-        slow, fast = head, head.next
+        slow = head
+        fast = head.next
 
-        while fast and fast.next :
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        
+
         second = slow.next
         slow.next = None
-
-        first = head
         second = self.reverse(second)
 
+        first = head
+
         while second:
-            tmp1, tmp2 = first.next, second.next
+            tmp1,tmp2 = first.next,second.next
             first.next = second
             second.next = tmp1
-            first, second = tmp1, tmp2
+            first,second = tmp1,tmp2
 
 
-    
     def reverse(self,head):
-
         prev = None
 
-        while head:
+        while head != None:
             temp = head.next
             head.next = prev
             prev = head
             head = temp
         
         return prev
-                
