fbfbcd89d7d21431ce53f9f31964fa7243f5fedc
Shubhang Khattar <shubhang1995khattar@gmail.com>
2023-11-07 11:35:01 -0500

    Time: 52 ms (47.56%), Space: 22.6 MB (6.17%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,4 +2,4 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         sentence = [c.lower() for c in s if c.isalnum()]
 
-        return sentence == list(reversed(sentence))
+        return sentence == sentence[::-1]
